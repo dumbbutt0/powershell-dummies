ebae614ba6a4bdbb44d0543666984b16bdb418c0
dumbbutt0 <134574598+dumbbutt0@users.noreply.github.com>
2024-06-15 22:01:47 -0700

    Update porty.ps1

--- porty.ps1
+++ porty.ps1
@@ -1,1 +1,1 @@
-$ports=(80,443,21,22);$ip=("127.0.0.1");foreach ($port in $ports) {try{$socket=New-Object System.Net.Sockets.TcpClient($ip,$port);} catch;if ($socket -eq $null) {echo $ip" is closed on port "$port;} else {echo $ip" is open on port "$port"!"; $socket =$null;}}
+$ports=(80,443,21,22);$ip=("127.0.0.1");foreach ($port in $ports) {try{$socket=New-Object System.Net.Sockets.TcpClient($ip,$port);} catch{};if ($socket -eq $null) {echo $ip" is closed on port "$port;} else {echo $ip" is open on port "$port"!"; $socket =$null;}}
